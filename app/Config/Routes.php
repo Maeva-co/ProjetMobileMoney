8d45c8ed4b4034fc5663085c4b78df6bf36dd3ae
Maeva-co <maevartmbts@gmail.com>
2026-07-20 13:38:15 +0300

    retrait

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,8 @@ namespace Config;
 use App\Controllers\client\UsersController;
 use App\Controllers\client\SoldeController;
 use App\Controllers\client\DepotController;
+use App\Controllers\client\RetraitController;
+
 
 /**
  * @var RouteCollection $routes
@@ -17,6 +19,9 @@ $routes->get('/logout', [UsersController::class, 'logout']);
 $routes->get('/client/solde', [SoldeController::class, 'index']);
 $routes->get('/client/depot', [DepotController::class, 'index']);
 $routes->post('/client/depot', [DepotController::class, 'store']);
+$routes->get('/client/retrait', [RetraitController::class, 'index']);
+$routes->post('/client/retrait', [RetraitController::class, 'store']);
+
 
 
 $routes->group('admin', ['filter' => 'auth:admin'], function ($routes) {
